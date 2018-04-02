0c385dac177c511af60d47626b687715305f7316
StefanVidakovic <vidakovi@ualberta.ca>
2018-04-01 22:08:11 -0600

    Nothing significant, added a potential method for parsing database info into useful structurefor caculating closure

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from Database import*
 
 def main():
     x = ["once", "twice", "twice", "thrice"]
@@ -6,6 +7,20 @@ def main():
         print(thing)
         y.add(thing)
     print(y)
+    #Experimenting with parsing the database output into something useful for calculating closure
+    db = Database()
+    fds = db.getFD("Person")
+    lst = list()
+    for fd in fds:
+        temp = fd.replace("{","")
+        temp = temp.replace("}","")
+        temp = temp.replace(";","")
+        lst.append(temp.split("=>"))
+        # temp = temp.split(";")
+    # for fd in temp:
+    #     t = fd.split("=>")
+    #     lst.append(t)
+    print(lst)
 
 if __name__ == "__main__":
     main()
