741160c0c2e33b4edd9fd82a2cfbee086e0dad99
StefanVidakovic <vidakovi@ualberta.ca>
2018-04-02 14:40:43 -0600

    Switched to more intuitive set operations as described in notes

--- Normaliser.py
+++ Normaliser.py
@@ -15,47 +15,49 @@ class Normaliser:
             BCNF = True
             for table in tables:
                 for fd in table[1]:
-                    print("FD",fd,"\n")
-                    print("Closure",self.getClosure(fd[0],table[1]),"\n")
-                    print("Attributes",table[0],"\n")
+                    # print("FD",fd,"\n")
+                    # print("Closure",self.getClosure(fd[0],table[1]),"\n")
+                    # print("Attributes",table[0],"\n")
                     if not (self.getClosure(fd[0], table[1]) >= table[0]):
                         BCNF = False
                         print("FD attr ",fd[0],"\n")
-                        print("FD R",fd[1],"\n")
-                        fds1 = [fd]
-                        table1 = [fd[0].union(fd[1]),fds1]
+                        # print("FD R",fd[1],"\n")
+                        decompFD = self.decomposeFDs(table[0],fd,table[1])
+                        decompAttr = self.decomposeAttributes(table[0],fd)
+
+                        table1 = [decompAttr[0],decompFD[0]]
                         # print("TABLE ",table[0],"\n")
-                        attrSet2 = self.decomposeAttributes(table[0],fd)
-                        fds2 = self.decomposeFDs(fd,table[1])
-                        table2 = [attrSet2,fds2]
+                        table2 = [decompAttr[1],decompFD[1]]
                         tables.remove(table)
                         tables.append(table1)
                         tables.append(table2)
                         break
                 break
+        print("Result", tables, "\n")
         #store new schemas in OutputRelationSchemas
         #if instances exist for 'name' create and populate tables for new schemas
         #check dependency conserving and tell user
     def decomposeAttributes(self,attrSet,fd):
-        attrSet = attrSet.difference(fd[1].difference(fd[0]))
-        return attrSet
+        return [fd[0].union(fd[1]),attrSet.difference(fd[1].difference(fd[0]))]
 
 
-    def decomposeFDs(self, fd, fdList): #remove attributes from fds of 'remainder table' ie
-        newfdList = fdList.remove(fd)   #given ABCD:{A->BCD,B->C} decompose into AD:{A->D} and BC:{B->C} where the former is remainder
-        attrSet = fd[0].union(fd[1])    #this function currently only decomposes the FD list
-        for item in newfdList:
-            temp = item[0]|attrSet
-            if temp:
-                for attr in temp:
-                    item[0].remove(attr)
-            temp = item[1]|attrSet
-            if temp:
-                for attr in temp:
-                    item[1].remove(attr)
-            if not item[0] and not item[1]: #if removing attributes left empy sets, remove it
-                newfdList.remove(item)
-        return newfdList
+    def decomposeFDs(self, attrSet, fd, fdList): #remove attributes from fds of 'remainder table' ie
+        decompAttr = self.decomposeAttributes(attrSet,fd)
+        quotientfdList = list()
+        remainderfdList = fdList
+
+        quotientAttrSet = decompAttr[0]
+        for item in fdList:
+            if item[0] <= quotientAttrSet:
+                quotientfdList.append(item)
+
+        remainderAttrSet = decompAttr[1]
+        for item in fdList:
+            if not (item[0] <= remainderAttrSet):
+                remainderfdList.remove(item)
+
+        return [quotientfdList,remainderfdList]
+
 
     def equivalentSets(self, set1, set2):
         return True
