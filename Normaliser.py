9afb1e512efc68bdfa48e615795037f2972710d7
StefanVidakovic <vidakovi@ualberta.ca>
2018-04-02 11:52:57 -0600

    Added the FD decomposition method

--- Normaliser.py
+++ Normaliser.py
@@ -17,12 +17,29 @@ class Normaliser:
                 for fd in table[1]:
                     if not self.getClosure(fd[0], table[1]) >= table[0]:
                         BCNF = False
-
+                        fds1 = [fd]
+                        table1 = [fd[0].union(fd[1]),fds1]
 
                         continue
         #store new schemas in OutputRelationSchemas
         #if instances exist for 'name' create and populate tables for new schemas
         #check dependency conserving and tell user
+
+    def decomposeFDs(self, fd, fdList): #remove attributes from fds of 'remainder table' ie
+        newfdList = fdList.remove(fd)   #given ABCD:{A->ABCD,B->C} decompose into AD:{A->D} and BC:{B->C} where the former is remainder
+        attrSet = fd[0].union(fd[1])    #this function currently only decomposes the FD list
+        for item in newfdList:
+            temp = item[0]|attrSet
+            if temp:
+                for attr in temp:
+                    item[0].remove(attr)
+            temp = item[1]|attrSet:
+            if temp:
+                for attr in temp:
+                    item[1].remove(attr)
+            if not item[0] and not item[1]:
+                newfdList.remove(item)
+        return newfdList
 
     def equivalentSets(self, set1, set2):
         return True
