f1aa8eda7765f5883fa918cae279bdbcbc879e4d
StefanVidakovic <vidakovi@ualberta.ca>
2018-04-03 00:50:38 -0600

    Creating and populating new tables done

--- Database.py
+++ Database.py
@@ -58,11 +58,61 @@ class Database():
         self.cursor.execute('''INSERT OR IGNORE INTO OutputRelationSchemas VALUES (?,?,?)''',(name,attributes,FDs))
         self.connection.commit()
 
-    def addDecomposedTable(self,originalName,newName,table):
+    def addDecomposedTables(self,originalName,tables,nameList):
         self.cursor.execute('''PRAGMA table_info({})'''.format(originalName))
         allrows = self.cursor.fetchall()
         typeDict = dict()
         for row in allrows:
-            typeDict[row[1]] = row[2]
+            typeDict[row[1]] = [row[2],row[5]]
             # print(row[1],row[2])
-        
+        for index in range(len(tables)):
+            table = tables[index]
+            self.cursor.execute('''DROP TABLE IF EXISTS {}'''.format(nameList[index]))
+            createStr = "CREATE TABLE " + nameList[index] + " ( "
+            for attr in list(table[0]):
+                createStr = createStr + attr +" "+typeDict[attr][0]+", "
+
+            pkStr = "PRIMARY KEY ("
+            primaryKey = ""
+            keyList = list(table[0])
+            count = 0
+            for i in range(len(keyList)):
+                if typeDict[keyList[i]][1]>0:
+                    if count == 0:
+                        pkStr = pkStr + keyList[i]
+                        primaryKey = primaryKey + keyList[i]
+                        count = count + 1
+                        continue
+                    pkStr = pkStr + "," + keyList[i]
+                    primaryKey = primaryKey + "," + keyList[i]
+                    count = count + 1
+            pkStr = pkStr + ")"
+
+            fpkStr = ""
+            if index > 0 and len(keyList)>1:
+                for fd in tables[index-1][1]:
+                    if table[0].intersection(fd[0])>=fd[0]:
+                        # fpkStr = ", FOREIGN KEY (" + table[0].intersection(fd[0]) + ") REFERENCES " + nameList[i-1]
+                        fpkStr = ", FOREIGN KEY ("
+                        sortedKeys = sorted(table[0].intersection(fd[0]))
+                        for key in sortedKeys:
+                            fpkStr = fpkStr + key
+                        fpkStr = fpkStr + ") REFERENCES " + nameList[index-1]
+            createStr = createStr + pkStr + fpkStr + ");"
+            print(createStr)
+            self.cursor.execute(createStr)
+            self.connection.commit()
+
+            attrList = sorted(table[0])
+            insertStr = "INSERT OR REPLACE INTO " + nameList[index]
+            columnStr = ""
+            for i in range(len(attrList)):
+                if i == len(attrList)-1:
+                    columnStr = columnStr + attrList[i]
+                    break
+                columnStr = columnStr + attrList[i] + ","
+            print(primaryKey)
+            selectStr = " SELECT DISTINCT " + columnStr + " FROM " + originalName + " GROUP BY " + primaryKey + ";"
+            insertStr = insertStr + selectStr
+            self.cursor.execute(insertStr)
+            self.connection.commit()
